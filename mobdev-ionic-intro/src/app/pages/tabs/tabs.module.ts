1f970c57f62a89677f155db4b32990a1a0ad7b1a
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2020-03-03 14:14:49 +0000

    Buttons created and back button

--- mobdev-ionic-intro/src/app/pages/tabs/tabs.module.ts
+++ mobdev-ionic-intro/src/app/pages/tabs/tabs.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
-import { TabsPageRoutingModule } from './tabs-routing.module';
-
 import { TabsPage } from './tabs.page';
+
+import { TabsPageRouterModule } from './tabs-page-router.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TabsPageRoutingModule
+    TabsPageRouterModule,
   ],
   declarations: [TabsPage]
 })
